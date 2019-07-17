28025fd6c67a6f1f8f88a1fb007393878509849e
Mortuza Hossain <mortuza.crid@gmail.com>
2019-07-17 09:47:34 +0600

    Start working with php and saperating the files with inc

--- index.php
+++ index.php
@@ -1,62 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Home | CRID-DAM ROBOTIC LABS</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-    
-    <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
-    <![endif]-->
-    
-    <link rel="shortcut icon" href="images/ico/favicon.ico">
-    <link rel="apple-touch-icon" sizes="144x144" href="images/ico/apple-touch-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="images/ico/apple-touch-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="images/ico/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" href="images/ico/apple-touch-icon-57x57.png">
-
-
-</head><!--/head-->
-<body>
-
-    <div class="preloader"> <i class="fa fa-circle-o-notch fa-spin"></i></div>
-
-
-    <header class="navbar navbar-inverse navbar-fixed-top " role="banner">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <i class="fa fa-bars"></i>
-                </button>
-                 <a class="navbar-brand" href="index.html"><h1>CRID-DAM ROBOTIC LABS</h1></a>
-            </div>
-            <div class="collapse navbar-collapse">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="http://criddam.com/training/">Training</a></li>
-                    <li><a href="http://criddam.com/robot-builders-community/">Community</a></li>
-                    <li><a href="http://criddam.com/about-us/">About Us</a></li>
-                    <li><a href="http://criddam.com/contact-us/">Contact</a></li>
-                    <li><a href="http://criddam.com/jobs/">Career</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Account<i class="icon-angle-down"></i></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="http://criddam.com/login/">Login</a></li>
-                            <li><a href="http://criddam.com/register/">Signup</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </header><!--/header-->
+<?php
+    include 'inc/header-home.php';
+    include 'inc/navbar.php';
+?>
 
     <section id="main-slider" class="no-margin">
         <div class="carousel slide" data-ride="carousel">
@@ -272,12 +217,4 @@
         <!--/#footer-->
     </div>
 
-    <script src="js/jquery.js"></script>
-    <script src="js/plugins.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/prettyPhoto/3.1.6/js/jquery.prettyPhoto.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/typeit/6.0.3/typeit.min.js"></script>
-    <script src="js/init.js"></script>
-    <script type="text/javascript" src="js/others.js"></script>
-</body>
-</html>
+<?php include 'inc/footer-home.php'; ?>
--- js/others.js
+++ js/others.js
@@ -1,21 +1,11 @@
 jQuery(document).ready(function($){
-'use strict';
-    jQuery('body').backstretch([
-        "images/bg/bg1.jpg",
-        "images/bg/bg2.jpg",
-        "images/bg/bg3.jpg"
-    ], {duration: 5000, fade: 500, centeredY: true });
-    var preloader = $('.preloader');
-    $(window).load(function(){
-        preloader.remove();
-    });
+  var preloader = $('.preloader');
+  $(window).load(function(){
+    preloader.remove();
+  });
 });
-new TypeIt('#replaceStrings', {
-  strings: ["Bring us your prototype", "Improvise it at our lab","We will fund it",
-  "We will help you to patent it","Market it","Be a successful Innovator"],
-  speed: 100,
-  breakLines: false,
-  waitUntilVisible: true
-}).go();
 
-$('#main-slider .carousel-content').flexVerticalCenter({ cssAttribute: 'padding-top' });
+lightbox.option({
+  'resizeDuration': 200,
+  'wrapAround': true
+});
