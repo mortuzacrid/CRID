d1e812903987638ccb58a492093c2c4eb89b3578
Mortuza Hossain <mortuza.crid@gmail.com>
2019-07-31 09:44:11 +0600

    Added details page in career

--- admin/career/career.php
+++ admin/career/career.php
@@ -20,27 +20,28 @@
           <div class="col-md-9">
 
 
-<?php
-    if (isset($_GET['p']) && $_GET['p'] < 0) {
-        $startpage = 1;
-    } elseif (isset($_GET['p']) ){
-        $startpage = ($_GET['p'] * POST_PER_PAGE) - POST_PER_PAGE;
-    } else {
-        $startpage = 0;
-    }
+            <?php
+                if (isset($_GET['p']) && $_GET['p'] < 0) {
+                    $startpage = 1;
+                } elseif (isset($_GET['p']) ){
+                    $startpage = ($_GET['p'] * POST_PER_PAGE) - POST_PER_PAGE;
+                } else {
+                    $startpage = 0;
+                }
 
-    $sql = "SELECT * FROM careers ORDER BY id DESC LIMIT $startpage,".POST_PER_PAGE;
-    $result = mysqli_query($con,$sql);
-    $countemais = mysqli_num_rows($result);
+                $sql = "SELECT * FROM careers WHERE status = 1 ORDER BY id DESC LIMIT $startpage,".POST_PER_PAGE;
+                $result = mysqli_query($con,$sql);
+                $countemais = mysqli_num_rows($result);
 
-?>
+            ?>
 
 
             <div class="panel panel-default">
                 <div class="panel-heading main-color-bg">
                     <h3 class="panel-title">Career Posts</h3>
                 </div>
-                <div class="panel-body home-panel">
+                <div class="panel-body home-panel carrierpanel">
+                    <h2>Actieve Jobs</h2>
 
                     <?php
 
@@ -60,9 +61,8 @@
                     <?php foreach ($careers as $key) { ?>
                         <tr>
                             <td><?php echo $key['position']; ?></td>
-                            <!-- glyphicon glyphicon-remove -->
                             <td><?php echo $key['vacancy']; ?></td>
-                            <td><a class="btn btn-default" href="viewemail.php?id=<?php echo $key['id'] ?>">View</a></td>
+                            <td><a class="btn btn-default" href="detailcareer.php?id=<?php echo $key['id'] ?>">View</a></td>
                         </tr>
 
                     <?php } ?>
@@ -74,34 +74,36 @@
                         No email send via contact form.
 
                     <?php } ?>
+                
+                    <div class="pagination text-center">
+                    <?php
+                        $sql = "SELECT id FROM careers";
+                        $total = mysqli_num_rows(mysqli_query($con,$sql));
+                        $total = ceil($total/POST_PER_PAGE);
+
+                        // Getting the current page number
+                        if (isset($_GET['p'])) {
+                            $current_page = $_GET['p'];
+                        } else {
+                            $current_page = 1;
+                        }
+
+                        for ($i=1; $i <= $total ; $i++) {
+
+                        if ($i == $current_page) {
+
+                    ?>
+                    <a class="actieve" href="detailcarrier.php?p=<?php echo $i; ?>"><?php echo $i; ?></a>
+                    <?php
+                        } else {
+                    ?>
+                        <a href="detailcarrier.php?p=<?php echo $i; ?>"><?php echo $i; ?></a>
+                    <?php } } ?>
+                    </div>
 
                 </div>
 
-                <div class="pagination text-center">
-                <?php
-                    $sql = "SELECT id FROM careers";
-                    $total = mysqli_num_rows(mysqli_query($con,$sql));
-                    $total = ceil($total/POST_PER_PAGE);
-
-                    // Getting the current page number
-                    if (isset($_GET['p'])) {
-                        $current_page = $_GET['p'];
-                    } else {
-                        $current_page = 1;
-                    }
-
-                    for ($i=1; $i <= $total ; $i++) {
-
-                    if ($i == $current_page) {
-
-                ?>
-                <a class="actieve" href="detailcarrier.php?p=<?php echo $i; ?>"><?php echo $i; ?></a>
-                <?php
-                    } else {
-                ?>
-                    <a href="detailcarrier.php?p=<?php echo $i; ?>"><?php echo $i; ?></a>
-                <?php } } ?>
-                </div>
+                
 
             </div>
 
--- admin/inc/sidebar.php
+++ admin/inc/sidebar.php
@@ -2,7 +2,12 @@
 	// Total unread email
 	$sql = "SELECT id FROM contactus WHERE seen = 0";
 	$result = mysqli_query($con,$sql);
-  $count_unreademails = mysqli_num_rows($result);
+  	$count_unreademails = mysqli_num_rows($result);
+
+  	// Total Actieve Job Post
+  	$sql = "SELECT id FROM careers WHERE status = 1";
+  	$result = mysqli_query($con,$sql);
+  	$count_job_post = mysqli_num_rows($result);
 
 ?>
 
@@ -15,6 +20,6 @@
 	<a href="/" class="list-group-item"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Pages <span class="badge">12</span></a>
 	<a href="/" class="list-group-item"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Posts <span class="badge">33</span></a>
 	<a href="/" class="list-group-item"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Users <span class="badge">203</span></a>
-	<a href="<?php echo SCRIPT_ROOT; ?>/admin/career/career.php" class="list-group-item"><span class="glyphicon glyphicon-bullhorn" aria-hidden="true"></span> Career <span class="badge">203</span></a>
+	<a href="<?php echo SCRIPT_ROOT; ?>/admin/career/career.php" class="list-group-item"><span class="glyphicon glyphicon-bullhorn" aria-hidden="true"></span> Career <span class="badge"><?php echo $count_job_post; ?></span></a>
 	<a href="<?php echo SCRIPT_ROOT; ?>/admin/seeusemail.php" class="list-group-item"><span class="glyphicon glyphicon-bell" aria-hidden="true"></span> Contact Us Email<span class="badge"><?php echo $count_unreademails; ?></span></a>
 </div>
