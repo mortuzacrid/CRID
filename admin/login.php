68cee1e3d04e2e78da3afd2d64c611f7413e6e2f
Mortuza Hossain <mortuza.crid@gmail.com>
2019-08-17 16:30:16 +0600

    Added security in admin login.

--- admin/login.php
+++ admin/login.php
@@ -5,7 +5,7 @@
 
   if (isset($_POST['login'])) {
     $email = $_POST['email'];
-    $password = $_POST['password'];
+    $password = md5($_POST['password']);
 
     $sql = "SELECT id,username FROM admins WHERE email = '$email' AND password ='$password'";
     $result = mysqli_query($con,$sql);
